e043d95ab31754db5cd375a8c628f6c6a1ee7abe
David Spies <dnspies@gmail.com>
2017-04-16 21:27:56 -0700

    Various other tools and results.

--- count_utils.py
+++ count_utils.py
@@ -1,4 +1,6 @@
+import collections
 import pandas as pd
+import re
 
 from candidates import candidates
 
@@ -26,3 +28,68 @@ def assign_votes(votetable, lothreshold, hithreshold):
     for c in cnames:
         result[remaining & ismark.loc[:, c]] = c
     return result
+
+def official_groups(official_csv):
+    votes = pd.read_csv(official_csv).sort_values(by="Precinct").reset_index(drop=True)
+    keys = votes.loc[:, "Precinct"]
+    bykey = collections.defaultdict(list)
+    for (i,key) in enumerate(keys):
+        sndword = key.split(' ')[1]
+        bykey[sndword].append(votes.loc[i, :])
+    return {k : pd.DataFrame(v).set_index('Precinct').fillna(0)
+            for (k, v) in bykey.items()}
+
+def audit_groups(store_file, lothreshold, hithreshold):
+    with pd.HDFStore(store_file) as store:
+        bykey = collections.defaultdict(list)
+        for t in store.tables:
+            key = t[len("wards/"):-len(".zip")]
+            sndword = key.split(" ")[0]
+            vs = assign_votes(store[t], lothreshold, hithreshold).value_counts()
+            vs["Precinct"] = key
+            bykey[sndword].append(vs)
+    return {k : pd.DataFrame(v).set_index('Precinct').fillna(0)
+            for (k, v) in bykey.items()}
+
+def parse_madison(name):
+    if name.startswith("Madison City Wards"):
+        rest = name[len("Madison City Wards"):]
+        ttype = "City"
+    elif name.startswith("Madison Town Wards"):
+        rest = name[len("Madison Town Wards"):]
+        ttype = "Town"
+    else:
+        raise Exception("Unknown name: " + name)
+    numstrs = re.split(',|&', rest)
+    nums = []
+    for s in numstrs:
+        ps = s.split('-')
+        if len(ps) == 1:
+            [x] = ps
+            nums.append(int(x.strip()))
+        elif len(ps) == 2:
+            [x,y] = ps
+            for i in range(int(x.strip()),int(y.strip())+1):
+                nums.append(i)
+        else:
+            raise Exception("Unknown nums: " + name)
+    return (nums, ttype)
+
+def madison_city_groups(official_madison, counted_madison):
+    sumofficial = []
+    inds = []
+    nums_unused = set(range(1,139))
+    for name in counted_madison.index:
+        (nums, ttype) = parse_madison(name)
+        if ttype == "Town":
+            continue
+        assert ttype == "City"
+        inds.append(name)
+        precincts = []
+        for num in nums:
+            if num not in nums_unused:
+                raise Exception(num + " already used")
+            nums_unused.remove(num)
+            precincts.append("C Madison Wd " + str(num))
+        sumofficial.append(official_madison.loc[precincts, :].sum(axis=0))
+    return (pd.DataFrame(sumofficial, index=inds), nums_unused)
